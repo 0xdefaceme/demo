0764d2f6fc8db7189f93f464ac2bc75a718f87d1
Tim Daubenschütz <tim.daubenschuetz@gmail.com>
2019-02-27 23:57:50 +0100

    Travis integration

---
+++ scripts/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Stops other branches then master from getting built and pushed.
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]; then exit 0; fi
+
+set -o errexit
+
+rm -rf public
+mkdir public
+
+# config
+git config --global user.email "auto@deploy.er"
+git config --global user.name "Travis CI"
+
+# build
+npm run build
+
+# deploy
+cd public
+git init
+git add .
+git commit -m "Automatic update from Travis"
+git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
