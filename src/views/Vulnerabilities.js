13d24da1dabb51e10621398478181fd61db18a02
Tim Daubenschütz <tim.daubenschuetz@gmail.com>
2019-02-28 23:31:15 +0100

    Make rinkeby testable

--- src/views/Vulnerabilities.js
+++ src/views/Vulnerabilities.js
@@ -208,6 +208,9 @@ class VulnerabilityList extends Component {
           </Cell>
           <Cell large={1} />
         </Grid>
+        Use these addresses for testing on Rinkeby:
+        0x2b1D000b5AdF0A7423727EfcC27878839DC0730E (Negotiator),
+        0x93a62ca8F119c7F1a87d05A1cc8CC353fd68a3E5 (Exploitable)
       </div>
     );
   }
--- truffle.js
+++ truffle.js
@@ -1,9 +1,22 @@
+//@format
+require("dotenv").config();
+const Web3 = require("web3");
+const web3 = new Web3();
+const Wallet = require("ethereumjs-wallet");
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
 module.exports = {
   networks: {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: '*'
+      network_id: "*"
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(process.env["RINKEBY_PRIVATE_KEY"], "https://rinkeby.infura.io"),
+      gas: 4600000,
+      gasPrice: web3.utils.toWei("20", "gwei"),
+      network_id: "4"
     }
   }
 };
