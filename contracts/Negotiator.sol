dff801ee1be05d409492c5c22a8e7ff2c3628cc4
Tim Daubenschütz <tim.daubenschuetz@gmail.com>
2019-03-26 14:31:09 +0100

    Use underscore in function parameters

--- contracts/Negotiator.sol
+++ contracts/Negotiator.sol
@@ -45,15 +45,15 @@ contract Negotiator {
     );
 
     function commit(
-        IExploitable exploitable
+        IExploitable _exploitable
     ) public returns (uint256 id) {
         require(
-            exploitable.implementsExploitable(),
+            _exploitable.implementsExploitable(),
             "Only exploitable with implementsExploitable can be committed"
         );
 
         id = vulns.push(Vuln({
-            exploitable: exploitable,
+            exploitable: _exploitable,
             attacker: msg.sender,
             key: "",
             bounty: 0,
@@ -63,11 +63,11 @@ contract Negotiator {
             reason: "",
             paidAt: 0
         })) - 1;
-        emit Commit(id, address(exploitable), msg.sender);
+        emit Commit(id, address(_exploitable), msg.sender);
     }
 
-    function pay(uint256 id, string memory key) public payable {
-        Vuln storage vuln = vulns[id]; 
+    function pay(uint256 _id, string memory _key) public payable {
+        Vuln storage vuln = vulns[_id]; 
         require(
             msg.sender == address(vuln.exploitable), 
             "Only exploitable can call pay"
@@ -77,65 +77,65 @@ contract Negotiator {
             "Only committed vulns can be paid"
         );
 
-        vuln.key = key; 
+        vuln.key = _key; 
         //solium-disable security/no-block-members
         vuln.paidAt = block.timestamp;
         vuln.bounty = msg.value;
         vuln.status = Status.Paid;
-        emit Pay(id, key, msg.value);
+        emit Pay(_id, _key, msg.value);
     }
 
     function reveal(
-        uint256 id,
-        string memory plain,
-        string memory encrypted
+        uint256 _id,
+        string memory _plain,
+        string memory _encrypted
     ) public {
-        Vuln storage vuln = vulns[id];
+        Vuln storage vuln = vulns[_id];
         require(vuln.status == Status.Paid, "Only paid vulns can be revealed");
         require(msg.sender == vuln.attacker, "Only attacker can reveal");
 
-        vuln.plain = plain;
-        vuln.encrypted = encrypted;
+        vuln.plain = _plain;
+        vuln.encrypted = _encrypted;
         vuln.status = Status.Revealed;
-        emit Reveal(id, plain, encrypted);
+        emit Reveal(_id, _plain, _encrypted);
     }
 
-    function decide(uint256 id, bool exit, string memory reason) public {
-        Vuln storage vuln = vulns[id];
+    function decide(uint256 _id, bool _exit, string memory _reason) public {
+        Vuln storage vuln = vulns[_id];
         require(
             msg.sender == address(vuln.exploitable), 
             "Only exploitable can decide"
         );
 
-        if (timedout(id)) {
+        if (timedout(_id)) {
             vuln.status = Status.Timeout;
             vuln.reason = "Attacker didn't reveal in time.";
             vuln.exploitable.restore.value(vuln.bounty)();
             vuln.bounty = 0;
-            emit Decide(id, false);
+            emit Decide(_id, false);
         } else if (vuln.status == Status.Revealed) {
-            if (exit) {
+            if (_exit) {
                 vuln.status = Status.Exited;
-                vuln.reason = reason;
+                vuln.reason = _reason;
                 vuln.exploitable.exit();
                 //solium-disable security/no-send
                 vuln.attacker.send(vuln.bounty);
                 vuln.bounty = 0;
-                emit Decide(id, true);
+                emit Decide(_id, true);
             } else {
                 vuln.status = Status.Declined;
-                vuln.reason = reason;
+                vuln.reason = _reason;
                 vuln.exploitable.restore.value(vuln.bounty)();
                 vuln.bounty = 0;
-                emit Decide(id, false);
+                emit Decide(_id, false);
             }
         } else {
             revert("Only paid or revealed vulns can be decided");
         }
     }
 
-    function timedout(uint256 id) public view returns (bool) {
-        Vuln storage vuln = vulns[id];
+    function timedout(uint256 _id) public view returns (bool) {
+        Vuln storage vuln = vulns[_id];
         return vuln.status == Status.Paid && 
             vuln.paidAt + timeout > block.timestamp;
     }
@@ -145,11 +145,11 @@ contract Negotiator {
     }
 
     function filter(
-        address exploitable
+        address _exploitable
     ) public view returns (uint256[] memory filtered) {
         uint256 count = 0;
         for(uint256 i = 0; i < vulns.length; i++) {
-            if (address(vulns[i].exploitable) == exploitable) {
+            if (address(vulns[i].exploitable) == _exploitable) {
                 count++;
             }
         }
@@ -158,7 +158,7 @@ contract Negotiator {
         count = 0;
         
         for (uint256 j = 0; j < vulns.length; j++) {
-            if (address(vulns[j].exploitable) == exploitable) {
+            if (address(vulns[j].exploitable) == _exploitable) {
                 filtered[count++] = j;
             }
         }
